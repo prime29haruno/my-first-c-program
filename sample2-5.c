f76be98ce08b2bd704663ae91603c09cccccf316
prime29haruno <prime29haruno@gmail.com>
2025-11-21 14:09:04 +0900

    Add sample2-5.c

---
+++ sample2-5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("10進数の10は%dです。\n", 10);
+    printf("8進数の10は%dです。\n", 010);
+    printf("16進数の10は%dです。\n", 0x10);
+    printf("16進数のF2A3は%dです。\n", 0xF2A3);
+
+    return 0;
+}

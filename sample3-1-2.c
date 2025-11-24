736215ebc04b268d614a55942a9014d110dc24b8
prime29haruno <prime29haruno@gmail.com>
2025-11-24 12:11:07 +0900

    Add sample3-1-2.c

---
+++ sample3-1-2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+
+    n = 3;
+
+    printf("変数nの値は%dです。\n", n);
+
+    int m;
+
+    m = n;
+
+    printf("変数mの値は%dです。\n", m);
+
+    return 0;
+}

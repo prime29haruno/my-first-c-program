1e308562413a1e4b9077de2a0d6f2a550f00a9b6
prime29haruno <prime29haruno@gmail.com>
2025-11-26 14:46:32 +0900

    Add sample4-7.c

---
+++ sample4-7.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int inum;
+    double dnum;
+
+    inum = 160;
+    printf("身長は%dセンチです。\n", inum);
+    dnum = inum;
+    printf("身長は%fセンチです。\n", dnum);
+
+    dnum = 160.5;
+    printf("身長は%fセンチです。\n", dnum);
+    inum = dnum;
+    printf("身長は%dセンチです。\n", inum);
+
+    return 0;
+}

8b03d2ffcd6673c5860e14e2d2f7db4069df3b42
prime29haruno <prime29haruno@gmail.com>
2025-11-26 15:03:36 +0900

    Add sample4-8-1.c

---
+++ sample4-8-1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    ///円の部分は省略
+
+    int num1 = 5, num2 = 4;
+    double div;
+
+    div = (double)num1/num2;
+    printf("%d/%dは%fです。\n", num1, num2, div);
+
+    return 0;
+
+
+}

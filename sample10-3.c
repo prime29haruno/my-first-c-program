8565047f812e0ab81a550aa4831c9f7c1939601c
prime29haruno <prime29haruno@gmail.com>
2025-12-01 13:51:41 +0900

    Add sample10-3.c

---
+++ sample10-3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void buy(int x)
+{
+    printf("%d万円の車を買いました。\n", x);
+}
+
+int main(void)
+{
+    int num;
+
+    printf("1台目はいくらの車を買いますか？\n");
+    scanf("%d", &num);
+
+    buy(num);
+
+    printf("2台目はいくらの車を買いますか？\n");
+    scanf("%d", &num);
+
+    buy(num);
+
+    return 0;
+}

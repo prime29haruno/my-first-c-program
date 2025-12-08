2526f2261d1b433667cdc83e383d47d391595dbd
prime29haruno <prime29haruno@gmail.com>
2025-12-08 15:53:38 +0900

    Add sample8-1.c

---
+++ sample8-1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+#define NUM 5
+
+int main(void)
+{
+    int test[NUM];
+    int tmp;
+    int s, t;
+
+    printf("%d人の点数を入力してください。\n", NUM);
+    for (s = 0; s < NUM; s++) {
+        scanf("%d", &test[s]);
+    }
+
+    for (s = 0; s < NUM - 1; s++) {
+        for (t = s + 1; t < NUM; t++) {
+            if (test[t] > test[s]) {
+                tmp = test[t];
+                test[t] = test[s];
+                test[s] = tmp;
+            }
+        }
+    }
+
+    for (s = 0; s < NUM; s++) {
+        printf("%d位の人の点数は%dです。\n", s + 1, test[s]);
+    }
+
+    return 0;
+}

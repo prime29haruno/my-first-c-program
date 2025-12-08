f06e64b7c095189ead67860b62db1fb9fc0ee348
prime29haruno <prime29haruno@gmail.com>
2025-12-08 14:38:26 +0900

    Add sample11-7.c

---
+++ sample11-7.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int a = 0;
+
+void func(void)
+{
+    int b = 0;
+    static int c = 0;
+
+    printf("変数aは%d, 変数bは%d, 変数cは%dです。\n", a, b, c);
+
+    a++;
+    b++;
+    c++;
+}
+
+int main(void)
+{
+    int i;
+
+    for (i = 0; i < 5; i++) {
+        func();
+    }
+
+    return 0;
+}

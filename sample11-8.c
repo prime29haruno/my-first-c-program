3441a9ceaa53e1351638e72961fdb3348b0a9f95
prime29haruno <prime29haruno@gmail.com>
2025-12-08 14:45:23 +0900

    Add sample11-8.c

---
+++ sample11-8.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int max(int x, int y);
+
+int main(void)
+{
+    int num1, num2, ans;
+
+    printf("1番目の整数を入力してください。\n");
+    scanf("%d", &num1);
+
+    printf("2番目の整数を入力してください。\n");
+    scanf("%d", &num2);
+
+    ans = max(num1, num2);
+
+    printf("最大値は%dです。\n", ans);
+
+    return 0;
+}
+
+int max(int x, int y)
+{
+    if (x > y) return x;
+    else return y;
+}

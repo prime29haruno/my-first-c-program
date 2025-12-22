0c6689400ed331013e1a988671205c2a02a2a351
prime29haruno <prime29haruno@gmail.com>
2025-12-22 13:28:15 +0900

    Add sample13-1.c

---
+++ sample13-1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+struct Car {
+    int num;
+    double gas;
+};
+
+int main(void)
+{
+    struct Car car1;
+
+    car1.num = 1234;
+    car1.gas = 25.5;
+
+    printf("車のナンバーは%d、ガソリン量は%.1fです。\n", car1.num, car1.gas);
+
+    return 0;
+}

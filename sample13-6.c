5d9300ea244f4463600a78a08af164fc0f7c40d9
prime29haruno <prime29haruno@gmail.com>
2025-12-22 13:57:16 +0900

    Add sample13-3.c

--- sample13-6.c
+++ sample13-6.c
@@ -3,26 +3,24 @@
 typedef struct Car {
     int num;
     double gas;
-    struct Car *next;
 } Car;
+
+void show(Car c)
+{
+    printf("車のナンバーは%d、ガソリン量は%.1fです。\n", c.num, c.gas);
+}
 
 int main(void)
 {
-    Car car0, car1, car2;
-    Car *pC;
+    Car car1;
 
-    car0.num = 1234; car0.gas = 25.5;
-    car1.num = 4567; car1.gas = 52.2;
-    car2.num = 7890; car2.gas = 20.5;
+    printf("ナンバーを入力してください。\n");
+    scanf("%d", &car1.num);
 
-    car0.next = &car1;
-    car1.next = &car2;
-    car2.next = NULL;
+    printf("ガソリン量を入力してください。\n");
+    scanf("%lf", &car1.gas);
 
-    for (pC = &car0; pC != NULL; pC = pC->next) {
-        printf("車のナンバーは%d、ガソリン量は%.1fです。\n", pC->num, pC->gas);
-    }
+    show(car1);
 
     return 0;
 }
-
